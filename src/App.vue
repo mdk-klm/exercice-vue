c19d5157f73a4c469108becbb56b644e9d126e72
mdk-klm <antoine.04@laposte.net>
2021-02-02 15:17:46 +0100

    replace a^^

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,123 @@
 <template>
-  <div class="container-fluid">
-    <div id="app">
-      <User />
-    </div>
+  <User msg="Welcome to Your Vue.js App" />
+  <div class="headt">
+    <button class="btn btn-primary" v-on:click="fetchUsers">Réupérer des utilisateurs</button>
+    <label>
+      <input
+        v-model="genderFilter"
+        type="checkbox"
+        value="male"
+      >
+      Hommes
+    </label>
+    <label>
+      <input
+        v-model="genderFilter"
+        type="checkbox"
+        value="female"
+      >
+      Femmes
+    </label>
+    <label>
+      Rechercher :
+      <input type="text" v-model="search" placeholder="Rechercher"/>
+    </label>  
+    <label>Trier par âge :          
+    </label>
+    <p v-if="sortDirection === ''">Par défaut</p>
+    <p v-if="sortDirection === 'asc'">Croissant</p>
+    <p v-if="sortDirection === 'desc'">Décroissant</p>
   </div>
+  <p v-if="users.length">il y a <strong>{{searchedUsers.length}}</strong> utilisateurs</p>
+  <p v-else >il n'y a <strong>aucun</strong> utilisateur</p>
+  <table class="table table-hover" v-if="users.length">
+  <thead>
+    <tr>
+        <th>Photo</th>
+        <th>Nom</th>
+        <th>Email</th>
+        <th>Tel</th>
+        <th>Genre</th>
+        <th> <button v-on:click="changeSort" class="btn btn-light"> Âge
+          <i v-if="sortDirection" class="fa" v-bind:class="[ sortDirection === 'asc' ? 'fa-sort-up' : 'fa-sort-down' ]"></i>
+        </button></th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in searchedUsers" :key="user.email">
+          <td><img :src="user.picture.thumbnail"></td>
+          <td>{{ user.name.first }} {{ user.name.last }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.phone }}</td>
+          <td>{{ user.gender }}</td>
+          <td>{{ user.dob.age }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
 <script>
-import User from "./components/User.vue";
+import User from './components/User.vue'
+import axios from 'axios'
+
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    User
-  }
-};
+    User,
+  },
+  data() {
+    return {
+      users: [],
+      errored: false,
+      genderFilter: ['male', 'female'],
+      search: '',
+      sortDirection: ''
+    }
+  },
+  
+  methods: {
+    fetchUsers() { 
+      axios
+        .get('https://randomuser.me/api/?results=20')
+        .then(response => {
+         this.users = [...this.users, ...response.data.results]
+         //this.users = this.users.concat(response.data.results)
+        })
+        .catch(error => {
+          console.error(error)
+          this.errored = true
+        })
+    },
+    changeSort() {
+      if (this.sortDirection === ''){
+        this.sortDirection = 'asc'
+        return this.users
+      }else if (this.sortDirection === 'asc'){
+        this.sortDirection = 'desc'
+      } else if (this.sortDirection === 'desc'){
+        this.sortDirection = ''
+      }
+    },
+  },
+  computed: {
+    searchedUsers() {
+      return this.users
+      .filter((user) => this.genderFilter.includes(user.gender))
+      .filter((user) => {
+        return (user.name.first.toLowerCase().includes(this.search.toLowerCase())) ||
+        (user.name.last.toLowerCase().includes(this.search.toLowerCase()))
+      })
+      .sort((a,b) => {
+        if (!this.sortDirection) return 0;
+        const  modifier = this.sortDirection === 'desc' ? -1 : 1;
+        return (a.dob.age - b.dob.age) * modifier;
+      })
+    },
+  },
+  created(){ this.fetchUsers()},
+  
+}
+
 </script>
 
 <style>
@@ -23,5 +128,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.headt{
+  display: flex;
+  justify-content: space-around;
+}
+.btn-primary{
+  background-color: #41B883!important;
+  border-color: #41B883!important;
+}
+.btn-primary:hover{
+  background-color: #35495E!important;
 }
 </style>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,152 +1,36 @@
 <template>
-  <div>
-    <h1>VueJS example</h1>
-    <hr />
-    <button id="fetch-users" @click="fetchUsers">
-      Fetch users
-    </button>
-    <input v-model="genderFilter" type="checkbox" value="male" />
-    Hommes
-    <input v-model="genderFilter" type="checkbox" value="female" />
-    Femmes
-    <div id="none">
-      {{ genderFilter }}
-    </div>
-    <div>
-      <input v-model="filterByName" placeholder="Rechercher un utilisateur" />
-    </div>
-    <table id="tbl-users" class="table table-hover">
-      <thead>
-        <tr>
-          <th />
-          <th>Nom</th>
-          <th>Email</th>
-          <th>Tel</th>
-          <th>Genre</th>
-          <th @click="sort('age')">
-            <div v-if="(currentSortDir = 'desc')">
-              Age
-              <svg
-                aria-hidden="true"
-                height="14"
-                width="14"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="sort-up"
-                class="svg-inline--fa fa-sort-up fa-w-10"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M279 224H41c-21.4 0-32.1-25.9-17-41L143 64c9.4-9.4 24.6-9.4 33.9 0l119 119c15.2 15.1 4.5 41-16.9 41z"
-                ></path>
-              </svg>
-            </div>
-            <div v-else>
-              <svg
-                aria-hidden="true"
-                height="14"
-                width="14"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="sort-down"
-                class="svg-inline--fa fa-sort-down fa-w-10"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z"
-                ></path>
-              </svg>
-            </div>
-          </th>
-        </tr>
-      </thead>
-      <tbody id="tbody-users">
-        <tr v-for="user in namesFiltered" :key="user.name">
-          <td><img :src="user.picture.thumbnail" /></td>
-          <td>{{ user.name.first }} {{ user.name.last }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.phone }}</td>
-          <td>{{ user.gender }}</td>
-          <td>{{ user.dob.age }}</td>
-        </tr>
-      </tbody>
-    </table>
-
-    <div id="none2">
-      debug: sort={{ currentSort }}, dir={{ currentSortDir }}
+  <div class="container-fluid">
+    <div id="test">
+        <div>
+            <h1>Vue app filter v1</h1>
+            <hr/>
+            
+            
+            <p id="nb-users"></p>
+        </div>
+        
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
-  name: "User",
+  name: 'User',
   data() {
     return {
-      results: [],
-      errored: false,
-      genderFilter: ["male", "female"],
-      filterByName: "",
-      currentSort: "age",
-      currentSortDir: "asc"
-    };
-  },
-  computed: {
-    usersFiltered() {
-      return this.results.filter(user =>
-        this.genderFilter.includes(user.gender)
-      );
-    },
-    namesFiltered() {
-      return this.usersFiltered.filter(user => {
-        return (user.name.first + " " + user.name.last)
-          .toLowerCase()
-          .includes(this.filterByName.toLowerCase());
-      });
-    },
-    agesFiltered() {
-      return this.namesFiltered.slice().sort((a, b) => {
-        let modifier = 1;
-        if (this.currentSortDir === "desc") modifier = -1;
-        if (a.dob.age < b.dob.age) return -1 * modifier;
-        if (a.dob.age > b.dob.age) return modifier;
-        return 0;
-      });
+      users: null
     }
   },
-  methods: {
-    fetchUsers() {
-      axios
-        .get("https://randomuser.me/api/?results=20")
-        .then(response => (this.results.push.apply(this.results, response.data.results))
-        .catch(error => {
-          console.log(error);
-          this.errored = true;
-        }));
-    },
-    sort(s) {
-      
-      if (s === this.currentSort) {
-        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
-      }
-      this.currentSort = s;
-    }
+  props: {
+    msg: String
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  margin: 20px 0 0;
-  color: purple;
+h3 {
+  margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
@@ -158,11 +42,5 @@ li {
 }
 a {
   color: #42b983;
-}
-#none {
-  display: none;
-}
-#none2 {
-  display: none;
 }
 </style>
