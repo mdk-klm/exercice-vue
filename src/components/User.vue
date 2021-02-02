e6a5965c77dda2588d0eb079514d41a59696ca7f
mdk-klm <antoine.04@laposte.net>
2021-02-02 15:46:00 +0100

    rework app and implement router

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,46 +1,173 @@
 <template>
-  <div class="container-fluid">
-    <div id="test">
-        <div>
-            <h1>Vue app filter v1</h1>
-            <hr/>
-            
-            
-            <p id="nb-users"></p>
-        </div>
-        
-    </div>
+  <div class="headt">
+    <button
+      class="btn btn-primary"
+      @click="fetchUsers"
+    >
+      Réupérer des utilisateurs
+    </button>
+    <label>
+      <input
+        v-model="genderFilter"
+        type="checkbox"
+        value="male"
+      >
+      Hommes
+    </label>
+    <label>
+      <input
+        v-model="genderFilter"
+        type="checkbox"
+        value="female"
+      >
+      Femmes
+    </label>
+    <label>
+      Rechercher :
+      <input
+        v-model="search"
+        type="text"
+        placeholder="Rechercher"
+      >
+    </label>  
+    <label>Trier par âge :          
+    </label>
+    <p v-if="sortDirection === ''">
+      Par défaut
+    </p>
+    <p v-if="sortDirection === 'asc'">
+      Croissant
+    </p>
+    <p v-if="sortDirection === 'desc'">
+      Décroissant
+    </p>
   </div>
+  <p v-if="users.length">
+    il y a <strong>{{ searchedUsers.length }}</strong> utilisateurs
+  </p>
+  <p v-else>
+    il n'y a <strong>aucun</strong> utilisateur
+  </p>
+  <table
+    v-if="users.length"
+    class="table table-hover"
+  >
+    <thead>
+      <tr>
+        <th>Photo</th>
+        <th>Nom</th>
+        <th>Email</th>
+        <th>Tel</th>
+        <th>Genre</th>
+        <th>
+          <button
+            class="btn btn-light"
+            @click="changeSort"
+          >
+            Âge
+            <i
+              v-if="sortDirection"
+              class="fa"
+              :class="[ sortDirection === 'asc' ? 'fa-sort-up' : 'fa-sort-down' ]"
+            />
+          </button>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="user in searchedUsers"
+        :key="user.email"
+      >
+        <td><img :src="user.picture.thumbnail"></td>
+        <td>{{ user.name.first }} {{ user.name.last }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.phone }}</td>
+        <td>{{ user.gender }}</td>
+        <td>{{ user.dob.age }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'User',
+  name: 'App',
+  components: {
+  },
   data() {
     return {
-      users: null
+      users: [],
+      errored: false,
+      genderFilter: ['male', 'female'],
+      search: '',
+      sortDirection: ''
     }
   },
-  props: {
-    msg: String
-  }
+  computed: {
+    searchedUsers() {
+      return this.users
+      .filter((user) => this.genderFilter.includes(user.gender))
+      .filter((user) => {
+        return (user.name.first.toLowerCase().includes(this.search.toLowerCase())) ||
+        (user.name.last.toLowerCase().includes(this.search.toLowerCase()))
+      })
+      .sort((a,b) => {
+        if (!this.sortDirection) return 0;
+        const  modifier = this.sortDirection === 'desc' ? -1 : 1;
+        return (a.dob.age - b.dob.age) * modifier;
+      })
+    },
+  },
+  created(){ this.fetchUsers()},
+  
+  methods: {
+    fetchUsers() { 
+      axios
+        .get('https://randomuser.me/api/?results=20')
+        .then(response => {
+         this.users = [...this.users, ...response.data.results]
+         //this.users = this.users.concat(response.data.results)
+        })
+        .catch(error => {
+          console.error(error)
+          this.errored = true
+        })
+    },
+    changeSort() {
+      if (this.sortDirection === ''){
+        this.sortDirection = 'asc'
+        return this.users
+      }else if (this.sortDirection === 'asc'){
+        this.sortDirection = 'desc'
+      } else if (this.sortDirection === 'desc'){
+        this.sortDirection = ''
+      }
+    },
+  },
+  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.headt{
+  display: flex;
+  justify-content: space-around;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.btn-primary{
+  background-color: #41B883!important;
+  border-color: #41B883!important;
 }
-a {
-  color: #42b983;
+.btn-primary:hover{
+  background-color: #35495E!important;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../components/Home.vue'
+import User from '../components/User.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/users',
+    name: 'User',
+    component: User
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
